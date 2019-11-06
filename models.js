b6c6ec8c5c025a6fb5ab2f5ed5f9224527e5f776
MrZHLF <480970024@qq.com>
2019-11-06 14:21:13 +0800

    登录注册接口编写

---
+++ models.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+// 链接数据库
+mongoose.connect('mongodb://localhost:27017/express-auth',{
+    useCreateIndex:true,
+    useNewUrlParser:true
+})
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:true //字段是否唯一
+    },
+    password:{
+        type:String,
+        set(val){
+            // 通过bcryptjs对密码加密返回值 第一个值返回值， 第二个密码强度
+            return require('bcryptjs').hashSync(val,10)
+        }
+    }
+})
+
+
+const User = mongoose.model('User',UserSchema)
+
+// 全部删除
+// User.db.dropCollection('users')
+module.exports = {User}
---
+++ server.js
@@ -0,0 +1,82 @@
+const {User} = require('./models')
+const express = require('express');
+const jwt = require('jsonwebtoken')
+const app = express();
+const SECRET = 'ewgfvwergvwsgw5454gsrgvsvsd'
+
+app.use(express.json())
+
+
+
+app.get('/api/test',async(req,res) =>{
+    res.send('ok')
+})
+
+
+app.get('/api/user',async(req,res) =>{
+    //查询所有用户
+    const users = await User.find()
+    res.send(users)
+})
+
+// 注册
+app.post('/api/register',async(req,res) =>{
+    const user = await User.create({
+        username:req.body.username,
+        password:req.body.password
+    })
+    // 返回出去
+    res.send(user)
+})
+
+
+// 登录
+app.post('/api/login',async(req,res) =>{
+    const user = await User.findOne({
+        username:req.body.username
+    })
+    if(!user) {
+        return res.status(422).send({
+            message:"用户不存在"
+        })
+    }
+
+    const isPasswordValid = require('bcryptjs').compareSync(
+        req.body.password,
+        user.password
+    )
+    if(!isPasswordValid){
+        return res.status(422).send({
+            message:"密码无效"
+        })
+    }
+    
+
+    const token = jwt.sign({
+        id:String(user._id)
+    },SECRET)
+
+    // 生成token
+    res.send({
+        user,
+        token
+    })
+})
+
+
+// 校验中间件
+const auth = async(req,res) =>{
+    const raw = String(req.headers.authorization).split(' ').pop();
+    // 验证
+    const {id} = jwt.verify(raw,SECRET)
+    req.user = await User.findById(id)
+}
+
+app.get('/api/profile',auth,async(req,res) =>{
+    res.send(req.user)
+})
+
+
+app.listen(3001,() =>{
+    console.log('http://localhost:3001')
+})
